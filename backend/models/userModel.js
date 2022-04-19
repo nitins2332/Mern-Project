38ccf5aa04853c3c3aa73757b1a16f0ac085739b
Nitins <nitins2332@gmail.com>
2022-04-19 14:38:35 +0530

    add login function, save token on cookies, Authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -57,4 +57,9 @@ userSchema.methods.getJWTToken = function () {
   });
 };
 
+// Compare Password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model("User", userSchema);
